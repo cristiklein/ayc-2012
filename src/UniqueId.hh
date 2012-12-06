43a9df584b2537174fda0359cc6ca444eab3b8d3
Cristian KLEIN <cristiklein@gmail.com>
2012-12-06 15:26:16 +0100

    Fixes in documentation

--- src/UniqueId.hh
+++ src/UniqueId.hh
@@ -5,41 +5,53 @@
 #include <unordered_map>
 #include <vector>
 
+/*! \brief Associate bulky names (e.g., strings)
+ * with light, unique identifiers (e.g., integers)
+ */
 template<class _Id = int, class _Name = std::string>
 class UniqueId
 {
 public:
-	typedef _Id Id;
-	typedef _Name Name;
+	typedef _Id Id; //!< Type of identifiers
+	typedef _Name Name; //!< Type of names
 
 	UniqueId()
 	{
-		/* Id zero is special */
+		/* The identifier zero is special, meaning identifier not yet registered */
 		idToName.emplace_back("");
 	}
 
+	/*!
+	 * \brief Retrieve the identifier or generate a new identifier for the given name
+	*/
 	Id getId(const Name &s)
 	{
+		/* Retrieve identifier from map */
 		Id &id = nameToId[s];
 
-		/* Not found, add */
+		/* If name was not found */
 		if (id == 0)
 		{
+			/* Generate a new identifier for it */
 			Id newId = idToName.size();
 			idToName.emplace_back(s);
 			id = newId;
 		}
+
 		return id;
 	}
 
+	/*!
+	 * \brief Retrieve the identifier or generate a new identifier for the given name
+	*/
 	Name getName(Id id)
 	{
 		return idToName[id];
 	}
 
 private:
-	std::unordered_map<Name, Id> nameToId;
-	std::vector<Name> idToName;
+	std::unordered_map<Name, Id> nameToId; //!< Hashmap to store name to identifier association
+	std::vector<Name> idToName; //!< Vector to store identifier to name association
 };
 
 #endif /* UNIQUE_ID */
