033f269cadcdda214d0e5d66fbb487ba57acdf41
Cristian KLEIN <cristiklein@gmail.com>
2012-12-06 15:41:57 +0100

    Added efficient class for storing flights

---
+++ src/Flights.hh
@@ -0,0 +1,84 @@
+#ifndef FLIGHTS_HH
+#define FLIGHTS_HH
+
+#include <map>
+#include <unordered_map>
+
+/*! \brief Stores information about a flight */
+template<
+	class _Id = int,
+	class _Time = unsigned long,
+	class _Airport = _Id,
+	class _Company = _Id,
+	class _Cost = float
+>
+struct Flight {
+	typedef _Id Id; //!< Type of flight identifier
+	typedef _Time Time; //!< Type of takeoff and landing time
+	typedef _Airport Airport; //!< Type of airport identifier
+	typedef _Company Company; //!< Type of company identifier
+	typedef _Cost Cost; //!< Type of cost
+
+	Id id; //!< Unique id of the flight
+	Airport from; //!< Airport where the flight takes off
+	Airport to; //!< Airport where the flight lands
+	Time takeoffTime; //!< Landing time
+	Time landTime; //!< Takeoff time
+	Company company; //!< Company of the flight
+	Cost cost; //!< Cost (without any discounts) of the flight
+};
+
+/*! \brief Stores flight schedules for the whole world
+ *
+ * It allows fast retrieval of flights from / to an airport
+ * taking off or landing in a given time interval.
+*/
+template<class _Flight = Flight<>>
+class Flights {
+public:
+	typedef _Flight Flight; //!< Type of flight
+	typedef typename Flight::Airport Airport; //!< Type of airport
+	typedef typename Flight::Time Time; //!< Type of time
+
+	typedef std::multimap<Time, Flight> AirportSchedule; //!< Type of index for fast retrieval of flights given a time interval
+	typedef std::unordered_map<Airport, AirportSchedule> Index; //!< Type of index for fast retrievel of flights given an airport
+	typedef typename AirportSchedule::iterator Iterator; //!< Type of iterator pointing to one flight
+	typedef std::pair<Iterator, Iterator> Range; //!< Type of range of flights
+
+	/*!
+	 * \brief Add a flight to the world
+	 */
+	void addFlight(const Flight &&flight)
+	{
+		takeoffsSchedule[flight.from][flight.takeoffTime] = flight;
+		landingsSchedule[flight.to][flight.landingTime  ] = flight;
+	}
+
+	/*!
+	 * \brief Get all flight from an airport taking off between tMin and tMax
+	 */
+	Range takeoffs(Airport airport, Time tMin, Time tMax)
+	{
+		const auto &takeoffsFromAirport = takeoffsSchedule[airport];
+		auto lower = takeoffsFromAirport.lower_bound(tMin);
+		auto upper = takeoffsFromAirport.upper_bound(tMax);
+		return { lower, upper };
+	}
+
+	/*!
+	 * \brief Get all flight from an airport taking off between tMin and tMax
+	 */
+	Range landings(Airport airport, Time tMin, Time tMax)
+	{
+		const auto &landingsOnAirport = landingsSchedule[airport];
+		auto lower = landingsOnAirport.lower_bound(tMin);
+		auto upper = landingsOnAirport.upper_bound(tMax);
+		return { lower, upper };
+	}
+
+private:
+	Index takeoffsSchedule; //!< Flights index by airport, then by takeoff time
+	Index landingsSchedule; //!< Flights index by airport, then by landing time
+};
+
+#endif /* FLIGHTS_HH */
